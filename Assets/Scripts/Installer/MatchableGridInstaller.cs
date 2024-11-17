433741ceb1d6eb67829e0415718696f74bf28cc8
Anil <anilhdas@gmail.com>
2024-11-17 23:17:22 +0200

    Cleanup Zenject Installer

--- Assets/Scripts/Installer/MatchableGridInstaller.cs
+++ Assets/Scripts/Installer/MatchableGridInstaller.cs
@@ -1,20 +1,14 @@
-using UnityEngine;
 using Zenject;
 
-using TapBlitz.Matchable;
 using TapBlitz.Grid;
 
 namespace TapBlitz.Installer
 {
     public class MatchableGridInstaller : MonoInstaller
     {
-        [SerializeField]
-        private TileGrid _tileGrid;
-
         public override void InstallBindings()
         {
-             Container.Bind<IMatchableGrid>().FromInstance(_tileGrid);
-            //Container.Bind<IMatchableGrid>().To<TileGrid>().FromComponentInHierarchy().AsCached();
+            Container.Bind<IMatchableGrid>().To<TileGrid>().FromComponentInHierarchy().AsCached();
         }
     }
 }
